fc78e4211493251b5b763805a34248a1a964f20d
Daniel R <nzdanron@gmail.com>
2024-09-14 14:40:30 +1200

    feat: basic form validation

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -26,18 +26,41 @@ const LoginForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const onLogin = () => {
-      console.log("Button clicked! " + username + " " + password)
+  const isBlank = (str: string) => {
+    return !str || /^\s*$/.test(str);
+  };
 
-      setPassword('')
-  }
+  const isDisabled = isBlank(username) || isBlank(password);
+
+  const onLogin = () => {
+    console.log("Button clicked! " + username + " " + password);
+
+    setPassword("");
+  };
 
   return (
     <div className="bg-white w-[400px] shadow-2xl rounded-lg p-6">
       <form action="none" className="flex flex-col gap-6">
-        <LoginInput placeholder='Username' value={username} setValue={setUsername} isPassword={false} />
-        <LoginInput placeholder='Password' value={password} setValue={setPassword} isPassword={true} />
-        <button type="button" className="rounded bg-blue-400 text-white p-2 shadow-md" onClick={onLogin}>Login</button>
+        <LoginInput
+          placeholder="Username"
+          value={username}
+          setValue={setUsername}
+          isPassword={false}
+        />
+        <LoginInput
+          placeholder="Password"
+          value={password}
+          setValue={setPassword}
+          isPassword={true}
+        />
+        <button
+          type="button"
+          className="rounded bg-blue-400 disabled:bg-blue-200 disabled:text-gray-50 transition-colors text-white p-2 shadow-md"
+          onClick={onLogin}
+          disabled={isDisabled}
+        >
+          Login
+        </button>
       </form>
     </div>
   );
