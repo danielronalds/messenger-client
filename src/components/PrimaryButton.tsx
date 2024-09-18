3815876fe47bf8ccf3b9b2c6dfeb8dfebd3a038e
Daniel R <nzdanron@gmail.com>
2024-09-18 14:20:52 +1200

    feat: gradient to PrimaryButton

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -2,7 +2,7 @@ const PrimaryButton = ({ desc, onClick, isDisabled }: { desc: string; onClick: (
   return (
     <button
       type="button"
-      className="rounded bg-blue-400 disabled:bg-blue-200 disabled:text-gray-50 transition-colors text-white p-2 shadow-md"
+      className="rounded bg-gradient-to-r from-cyan-500 to-blue-500 disabled:bg-gradient-to-r disabled:from-cyan-400 disabled:to-blue-400 disabled:text-gray-50 transition-colors text-white p-2 shadow-md"
       onClick={onClick}
       disabled={isDisabled}
     >
